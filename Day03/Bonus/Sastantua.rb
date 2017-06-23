d8925721e41030dc7eae59af8328aa1dc5a590ef
U-DESKTOP-R86USMS\Jay Vishwarupe <jayvishwarupe@gmail.com>
2017-06-23 00:58:31 -0700

    updated sastantua

--- Day03/Bonus/Sastantua.rb
+++ Day03/Bonus/Sastantua.rb
@@ -1,27 +1,84 @@
 #!/usr/bin/ruby
 def layercounter(x)
     layers = 0
-    val = Integer(x)
+    val = x.to_i
     for repeat in 1...val+1
         for num in 1..repeat+2
             layers+=1
         end
     end
-    puts layers
+    return layers
 end
 
+#star only count
+#5 13 23 35
+
+#model cubic func
+#5 17 31 49
+#first 12 15 18
+#second 3 3 3
+
+#4 9 15
 def sigma(layerval)
-    sum=0
-    for num in 1..layercounter(layerval)
-        sum+=1+num*2
-    end 
-    for i in 1..layerval
-        sum+=6
+    sum=1
+	val=(layercounter(layerval))
+    for num in 1...val
+        sum+=2
     end
-    return sum
+
+	if layerval.to_i > 1
+		#sum+=3*(layerval.to_i+2)
+		change=0
+		counter=0
+		for i in 1...layerval.to_i
+			layer_height = i+2
+			if layer_height%2==1
+				counter+=1
+				change = 3+counter-2
+			else
+				change = 3+counter-2
+			end
+			sum+=change*2
+			layer_height=0
+			change=0
+		end
+	end
+
+#    for i in 1...layerval.to_i
+#       sum+=6
+#   end
+    puts sum
 end
 
-layercounter(ARGV[0])
+def storeValues(x)
+	layer_count=x.to_i
+	values=[]
+	counter_height=3
+	star_sum = 1
+	counter_change = 0
+	for l in 1..layer_count
+		if l==1
+			values.push(star_sum)
+		end
+		for r in 1...counter_height
+			star_sum+=2
+			values.push(star_sum)
+		end
+		if counter_height%2==1
+			counter_change+=1
+			star_sum += 2*(3+counter_change-1)
+		else
+			star_sum += 2*(3+counter_change-1)
+		end	
+		if l!=layer_count
+			values.push(star_sum)
+		end
+		counter_height+=1
+	end
+	p values
+end
+
+storeValues(ARGV[0])
 
 =begin
 if(ARGV[0]==0)
