37f9808350916ab7299beb07f778cf5aababa885
U-DESKTOP-R86USMS\Jay Vishwarupe <jayvishwarupe@gmail.com>
2017-06-25 23:53:19 -0700

    Working Final Sastantua

--- Day03/Bonus/Sastantua.rb
+++ Day03/Bonus/Sastantua.rb
@@ -1,55 +1,4 @@
 #!/usr/bin/ruby
-def layercounter(x)
-    layers = 0
-    val = x.to_i
-    for repeat in 1...val+1
-        for num in 1..repeat+2
-            layers+=1
-        end
-    end
-    return layers
-end
-
-#star only count
-#5 13 23 35
-
-#model cubic func
-#5 17 31 49
-#first 12 15 18
-#second 3 3 3
-
-#4 9 15
-def sigma(layerval)
-    sum=1
-	val=(layercounter(layerval))
-    for num in 1...val
-        sum+=2
-    end
-
-	if layerval.to_i > 1
-		#sum+=3*(layerval.to_i+2)
-		change=0
-		counter=0
-		for i in 1...layerval.to_i
-			layer_height = i+2
-			if layer_height%2==1
-				counter+=1
-				change = 3+counter-2
-			else
-				change = 3+counter-2
-			end
-			sum+=change*2
-			layer_height=0
-			change=0
-		end
-	end
-
-#    for i in 1...layerval.to_i
-#       sum+=6
-#   end
-    puts sum
-end
-
 def storeValues(x)
 	layer_count=x.to_i
 	values=[]
@@ -100,7 +49,7 @@ def printPyramid(values, x)
 		if num > values.length-pyramid_height
 			for i in 0...values[num-1]
 				if door_star_counter >= ((values[num-1]/2+1)-door_star-1) && door_star_counter <= ((values[num-1]/2+1)+door_star-gap_right)
-					if pyramid_height>2 && num.to_f == (values.length-pyramid_height/2).round && door_star_counter >= (values[num-1]/2+1)-door_star-gap_right-1 && door_star_counter <= (values[num-1]/2+1)+door_star-gap_right-1
+					if pyramid_height>2 && num.to_f == (values.length-pyramid_height/2).round && door_star_counter >= (values[num-1]/2+1).round+door_star-gap_right-1 && door_star_counter <= (values[num-1]/2+1)+door_star-gap_right-1
 						print "$"
 					else
 						print "|"
